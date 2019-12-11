bb786feb3cbffe2f84c00f1480f2ec78a76570d0
So0ni <173976914@qq.com>
2019-12-11 10:19:31 +0800

    [style] code format

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 import os
-from setuptools import setup, find_packages
+from setuptools import setup
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(ROOT, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='logging_color',
     version='0.1.2',
     description='a color patch for python standard logging module',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license='GPL Licence',
     author='Sonic Young',
     author_email='173976914@qq.com',
